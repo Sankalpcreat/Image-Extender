fbb6002f543702bb68eb44160afd623cc175a6ec
Sankalp Singh <singhsankalp997@gmail.com>
2024-10-29 00:52:55 +0530

    changes

--- backend/src/routes/authRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { googleLogin } from "../controller/authController";
-
-const router=Router();
-
-router.post("/google",googleLogin);
-export default router;
